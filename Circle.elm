b3374dcb64288b2651a111020bcd6bad467b11bd
Rahel Lüthy <rahel.luethy@gmail.com>
2015-09-17 14:21:19 +0200

    Add circle illusion

--- Circle.elm
+++ Circle.elm
@@ -1,25 +1,72 @@
-module Circle where
-
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
-import Mouse
+import Time exposing (..)
 import Window
+import List exposing (..)
+import AnimationFrame
 
+-- MODEL
 
-main : Signal Element
-main =
-  Signal.map2 scene Mouse.position Window.dimensions
+size = 600
+circleSize = 240
+dotCount = 12
+dotSize = 10
+velocity = 0.01
 
+type alias Dot =
+    { index : Int
+    , x : Float
+    , y : Float
+    }
 
-scene : (Int,Int) -> (Int,Int) -> Element
-scene (x,y) (w,h) =
-  let
-    (dx,dy) =
-      (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
-  in
-    collage w h 
-      [ circle 10
-          |> filled blue
-          |> move (dx, dy)
-      ]
+defaultDot : Dot
+defaultDot =
+    { index = 0
+    , x = 0.0
+    , y = 0.0
+    }
+
+type alias State = { dots : List Dot }
+
+defaultState : State
+defaultState = { dots = map (\i -> { defaultDot | index <- i}) [ 0 .. dotCount - 1 ] }
+
+-- UPDATE
+
+update : Time -> State -> State
+update time state = { state | dots <- map (moveDot time) state.dots }
+
+moveDot : Time -> Dot -> Dot
+moveDot time dot =
+  let s = velocity * time / pi
+      offset = toFloat dot.index * pi / toFloat dotCount
+      newX = (-circleSize + dotSize) * cos(s + offset)
+  in { dot | x <- newX }
+
+-- VIEW
+
+view : State -> Element
+view state =
+   let background = filled black (circle circleSize)
+       dotLinePairs = map viewDotWithLine state.dots
+   in collage size size (background :: dotLinePairs)
+
+viewDot : Dot -> Form
+viewDot d = alpha 0.8 (filled lightOrange (circle dotSize)) |> move (d.x, d.y)
+
+viewDotWithLine : Dot -> Form
+viewDotWithLine dot =
+  let dotView = viewDot dot
+      lineView = traced (solid white) (path [ (-size / 2.0, 0) , (size / 2.0, 0) ])
+      dotAndLineView = group [dotView , lineView]
+  in dotAndLineView |> rotate (toFloat dot.index * (pi / dotCount))
+
+-- SIGNALS
+
+main = Signal.map view animate
+
+animate : Signal State
+animate = Signal.foldp update defaultState time
+
+time = Signal.foldp (+) 0 AnimationFrame.frame
